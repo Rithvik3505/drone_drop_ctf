432076b5a325ced0480a10849cb970aab5ccd85b
rithvik <rithvikhemanth360@gmail.com>
2025-06-23 23:17:43 +0530

    code changed

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ from flask import Flask, render_template, send_from_directory
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def index():
     return render_template("index.html")
 
-@app.route('/download')
+@app.route("/download")
 def download_image():
     return send_from_directory("static", "drone_view.jpg", as_attachment=True)
 
-if __name__ == '__main__':
-    app.run(debug=False)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
